d260880061e5d63c3b9152f541556bb2915957ef
enjoycoding <34410341+joysoul@users.noreply.github.com>
2023-06-01 18:16:39 +0000

    Update example.ts and fly.toml

--- example.ts
+++ example.ts
@@ -1,12 +1,12 @@
 import {
   app,
+  contentType,
   get,
   post,
   redirect,
-  contentType,
 } from "https://denopkg.com/syumai/dinatra/mod.ts";
 
-const greeting = "<h1>Hello From Deno on Fly!</h1>";
+const greeting = "<h1>Hello 222 From Deno on Fly!</h1>";
 
 app(
   get("/", () => greeting),
