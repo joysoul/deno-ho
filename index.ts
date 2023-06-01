cea6fbcead6d9df2e18487b5346ec30201191551
enjoycoding <34410341+joysoul@users.noreply.github.com>
2023-06-01 17:42:33 +0000

    Update 5 files

--- index.ts
+++ index.ts
@@ -3,6 +3,20 @@ import { serve } from "https://deno.land/std/http/server.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("1aaa Hello Deno!"));
+app.get("/", async (c) => {
+  const client = Deno.createHttpClient({
+    proxy: { url: "http://123.60.143.98:7890" },
+  });
+  //const response = await fetch("https://myserver.com", );
+
+  let ssx = "https://api-ipv4.ip.sb/ip ";
+  let ss = await fetch("https://api-ipv4.ip.sb/ip ");
+  // let s = await ss.json();
+  let xx = await ss.text();
+  console.log("do:" + ssx);
+  console.log(xx);
+  return fetch(ssx, { client });
+  //c.text(xx);
+});
 
 serve(app.fetch);
